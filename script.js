85aa09e5af8189510e8e04132d57855cf6e8bef8
Thanooz45 <webvinni66@gmail.com>
2025-01-01 19:49:36 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let item=document.getElementById('clickMe');
+function clicking(){
+    item.style.backgroundColor="blue";
+    item.textContent="button clicked";
+    item.style.color="green";
+};
+let p1info=document.getElementById("p1but");
+let bids1=document.getElementById("bd1");
+function p1button(){
+    bids1.style.backgroundColor="red";
+};
+let p2info=document.getElementById("p2but");
+let bids2=document.getElementById("bd2");
+function p2button(){
+    bids2.style.backgroundColor="green";
+}
+let p3info=document.getElementById("p3but");
+let bids3=document.getElementById("bd3");
+function p3button(){
+    bids3.style.backgroundColor="yellow";
+}
+
